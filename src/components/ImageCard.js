1eea4f232d9ed90c020821ef2075d0c791699fae
GUNJAN1999 <gunjanbbps62@gmail.com>
2020-10-20 19:59:46 +0530

    v2 completed

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class ImageCard extends React.Component
 {
+	constructor(props)
+	{
+		super(props);
+		this.imageRef=React.createRef();
+	}
 	render(){
 		return(
 			<div>
 				<img
+				ref={this.imageRef}
 				src={this.props.image.urls.regular} 
 				alt={this.props.image.description}/>
 			</div>
